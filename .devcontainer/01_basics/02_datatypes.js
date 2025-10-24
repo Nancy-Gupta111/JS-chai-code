c2e33ce0d911246c35932bb4d7c0a56388801d06
Nancy Gupta <nancy831890@gmail.com>
2025-10-24 19:42:14 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict";/* treat all js code as newer version*/
+// alert(3 + 3)we are using node js not browser
+
+console.log(3+3) //code readibility should be high
+console.log("nancy")
+
+let name ="nancy"
+let age=20
+let isLoggedIn=false
+
+//number = 2 to power 53
+//bigint
+//string=""
+//boolean=true/false
+//null=empty value
+//undefined = value not assigned
+//undefined =
+//symbol = unique
+
+
+//object
+
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
