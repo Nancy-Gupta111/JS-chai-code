deec6170d88c3b30f7a08f06af2978cb1b3eae1f
Nancy Gupta <nancy831890@gmail.com>
2025-10-24 20:50:25 +0000

    convertion of datatypes

---
+++ .devcontainer/01_basics/03_conversion.js
@@ -0,0 +1,25 @@
+// let score = "33"
+
+//console.log(typeof score)
+
+ let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+//"33" = 33
+//"33abc" =Nan
+//true = 1; false=0
+//null=0
+//undefined = Nan
+
+let isLoggedIN= "nancy"
+let booleanIsLoggedIn = Boolean(isLoggedIN)
+console.log(booleanIsLoggedIn)
+
+//1=true; 0=false
+//"" =false
+//"nancy"=true
+let someNumber = 33
+let stringNumber = String(someNumber)
+comsole.log(stringNumber)
+console.lig(typeof stringNumber)
