19fe5ddd2db49e69d1dd27dc706b6b207cec16f2
Nancy Gupta <nancy831890@gmail.com>
2025-10-24 19:43:26 +0000

    datatypes

---
+++ .devcontainer/01_basics/01_var.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "nancy@google.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountState;
+// accountId = 2//not allowed
+accountEmail="ng@ng.com"
+accountPassword = "21212121"
+accountCity="bengaluru"
+
+console.log(accountId)
+console.table([accountId, accountEmail,accountState, accountPassword, accountCity]);
+
+
+/*prefer not to use var because of issues in block scope and function scope
+*/
