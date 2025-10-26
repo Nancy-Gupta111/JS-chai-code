b26de4306e87b650b2c83eaa09991edd9fe9a6b8
Nancy Gupta <nancy831890@gmail.com>
2025-10-26 04:47:07 +0000

    conversions

--- .devcontainer/01_basics/03_conversion.js
+++ .devcontainer/01_basics/03_conversion.js
@@ -20,6 +20,38 @@ console.log(booleanIsLoggedIn)
 //"" =false
 //"nancy"=true
 let someNumber = 33
-let stringNumber = String(someNumber)
-comsole.log(stringNumber)
-console.lig(typeof stringNumber)
+// let stringNumber = String(someNumber)
+// comsole.log(stringNumber)
+// console.lig(typeof stringNumber)
+
+
+//******************* OPERATIONS *******************/
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+//console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(10/2)
+// console.log(2%3)
+
+let str1 = "hello"
+let str2 = "world"
+ let str3 = str1+str2
+//  console.log(str3)
+
+//  console.log("1" + 2)
+//  console.log(1 + "2")
+//  console.log("1" + 2 + 2)
+//  console.log(1 + 2 + "2")
+//  console.log((3+4) * 5 % 3)
+
+//  console.log(+true)
+//  console.log(+"")
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter)
