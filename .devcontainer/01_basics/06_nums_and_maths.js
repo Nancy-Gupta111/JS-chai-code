d241a5cf03c8eb185dd2e09ccaccc18dcfec1aa0
Nancy Gupta <nancy831890@gmail.com>
2025-10-27 14:02:41 +0000

    math and num

---
+++ .devcontainer/01_basics/06_nums_and_maths.js
@@ -0,0 +1,28 @@
+const score = 400;
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance)
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(1))
+
+const otherNUmber = 123.8966
+console.log(otherNUmber.toPrecision(4))//123.9
+
+const hundred = 100000
+console.log(hundred.toLocaleString('en-IN'));
+
+//const Maths = Math;
+//console.log(Maths.floor(4.9))
+console.log(Math.ceil(4.2))
+console.log(Maths.min(4,3,6,8))
+console.log(Math.floor (Maths.random()*10 )+ 1)
+
+const min = 10
+const max = 20
+
+console.log(Maths.floor(Math.random) * (max-min + 1))
+
+
+
